8e0d0232a77430f8c53040055348b95c20b8d922
Nick <nick@pop-os.localdomain>
2022-12-24 10:47:23 -0500

    add example svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css';
+import ReactIcon from './react.svg';
 
 interface AppProps {
   testCopy: string;
@@ -7,6 +8,7 @@ interface AppProps {
 export function App(props: AppProps) {
   return (
     <>
+      <ReactIcon />
       <h1 className={styles.header}>{props.testCopy}</h1>
       <p>I am working!</p>
     </>
