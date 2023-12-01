c82fdc09d14d0c77dae218d905347bc87648f70e
Nick Carbone <drumnickydrum@gmail.com>
2023-12-01 18:10:40 -0500

    add .env example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import styles from './App.module.css';
-import ReactIcon from './assets/react.svg';
+import styles from "./App.module.css";
+import ReactIcon from "./assets/react.svg";
 
 interface AppProps {
   testCopy: string;
@@ -12,9 +12,10 @@ export function App(props: AppProps) {
       <h1 className={styles.header}>{props.testCopy}</h1>
       <h2>Build Checklist</h2>
       <ol>
-        <li>{'React Icon (svg loader)'}</li>
-        <li>{'Blue header (css modules)'}</li>
-        <li>{'Open Sans (font loader)'}</li>
+        <li>{"React Icon (svg loader)"}</li>
+        <li>{"Blue header (css modules)"}</li>
+        <li>{"Open Sans (font loader)"}</li>
+        <li>{`process.env.secret=${process.env.secret} (dot env plugin)`}</li>
       </ol>
     </>
   );
