b321a524c8c6e12af4b8e1aa4693bfccf865cab2
Nick Carbone <drumnickydrum@gmail.com>
2023-12-02 12:49:59 -0500

    fix scroll behavior

--- debug/components/ConsoleOutput/ConsoleOutput.tsx
+++ debug/components/ConsoleOutput/ConsoleOutput.tsx
@@ -1,34 +1,32 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ConsoleOutput.module.css";
 import Ansi from "ansi-to-react";
 import { Output } from "../../script-server/script-server.types";
 import clsx from "clsx";
 
 export function ConsoleOutput({ output }: { output: Output | undefined }) {
+  const [rootEl, setRootRef] = useState<HTMLDivElement | null>(null);
   const [codeLines, setCodeLines] = useState<string[]>([]);
-  const lastLineRef = useRef<HTMLDivElement>(null);
 
+  // Add output each time it's received
   useEffect(() => {
     if (output) {
       const newLine = String.fromCharCode(...(output?.data || []));
       setCodeLines((prevLines) => [...prevLines, newLine]);
-      setTimeout(() => {
-        lastLineRef.current?.querySelector("code")?.scrollIntoView();
-      }, 200);
     }
-  }, [output]);
+  }, [output, rootEl]);
+
+  // Keep the console scrolled to the bottom
+  useEffect(() => {
+    const top = rootEl?.scrollHeight ?? 0;
+    rootEl?.scrollTo({ top });
+  }, [codeLines.length, rootEl]);
 
   return (
-    <div className={clsx(styles.console)}>
+    <div ref={setRootRef} className={clsx(styles.console)}>
       <pre>
         {codeLines.map((line, i) => {
-          // attach the ref to the last line so we can scroll the console
-          const ref = i === codeLines.length - 1 ? lastLineRef : null;
-          return (
-            <div ref={ref} key={line + i}>
-              <Ansi>{line}</Ansi>
-            </div>
-          );
+          return <Ansi key={line + i}>{line}</Ansi>;
         })}
       </pre>
     </div>
